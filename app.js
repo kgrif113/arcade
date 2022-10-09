fc8460e31ed82dfb33c86308e59676b66253531d
Kenny <kennygrifana113@gmail.com>
2022-10-09 10:22:42 -0400

    still trying

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-let gameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
+let cells = document.getElementsByClassName("cell");
+let statusText = document.getElementById("statusText");
+let restartButton = document.getElementById("resetBtn");
+let winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
-let gameContainer = document.getElementById("game-container");
-for (let i = 0; i < gameBoard.length; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-  row.id = i;
-  gameBoard[i].forEach((el, idx) => {
-    row.innerHTML += `
-    <div id = 'row${i}col${idx}' class = 'column'>
-    `;
-  });
-  gameContainer.appendChild(row);
-}
-function handleCellClick() {
-  console.log(event.target);
+let options = ["", "", "", "", "", "", "", "", ""];
+let currPlayer = "X";
+let running = false;
 
-  gameContainer.addEventListener("click", handleCellClick);
+startGame();
+
+function startGame() {
+  cells.forEach((cell) => cell.addEventListener("click", cellClicked));
+  restartButton.addEventListener("click", resetGame);
+  statusText.textContent = `${currPlayer}'s turn!`;
+  running = true;
 }
+function cellClicked() {
+  const cellIndex = this.getAttribute("cellIndex");
+
+  if (options[cellIndex] != "" || !running) {
+    return;
+  }
+  updateCell(this, cellIndex);
+  checkWinner();
+}
+function updateCell(cell, index) {
+  options[index] = currPlayer;
+  cell.textContent = currPlayer;
+}
+function changePlayer() {}
+function checkWinner() {}
+function resetGame() {}
